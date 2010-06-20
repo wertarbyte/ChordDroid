e44e2d31f1e7380b1c298c3e5a3b840a73d0fba3
Stefan Tomanek <stefan@pico.ruhr.de>
2010-06-20 23:53:02 +0200

    add label to ChordComponent class

--- src/de/wertarbyte/chorddroid/harmony/ChordComponent.java
+++ src/de/wertarbyte/chorddroid/harmony/ChordComponent.java
@@ -7,28 +7,17 @@ package de.wertarbyte.chorddroid.harmony;
 import java.util.HashMap;
 
 public class ChordComponent {
-	public static final ChordComponent MAJOR = new ChordComponent( stepsToOffsets(Scale.MAJOR, new int[] {1, 3, 5}));
-	public static final ChordComponent MINOR = new ChordComponent(stepsToOffsets(Scale.MINOR, new int[] {1, 3, 5}));
-	public static final ChordComponent SUS4= new ChordComponent(stepsToOffsets(Scale.MAJOR, new int[] {1, 4, 5}));
-	public static final ChordComponent SUS2= new ChordComponent(stepsToOffsets(Scale.MAJOR, new int[] {1, 2, 5}));
-	public static final ChordComponent POWER= new ChordComponent(stepsToOffsets(Scale.MAJOR, new int[] {1, 5}));
-	public static final ChordComponent AUGMENTED = new ChordComponent(new int[] {1, 4, 8});
-	public static final ChordComponent DIMINISHED = new ChordComponent(new int[] {1, 3, 6});
-	public static final ChordComponent DOM_SEVENTH = new ChordComponent(stepsToOffsets(Scale.MINOR, new int[] {7}));
-	public static final ChordComponent SIXTH = new ChordComponent( stepsToOffsets(Scale.MAJOR, new int[] {6}));
+	public static final ChordComponent MAJOR = new ChordComponent("", stepsToOffsets(Scale.MAJOR, new int[] {1, 3, 5}));
+	public static final ChordComponent MINOR = new ChordComponent("m", stepsToOffsets(Scale.MINOR, new int[] {1, 3, 5}));
+	public static final ChordComponent SUS4 = new ChordComponent("sus4", stepsToOffsets(Scale.MAJOR, new int[] {1, 4, 5}));
+	public static final ChordComponent SUS2 = new ChordComponent("sus2", stepsToOffsets(Scale.MAJOR, new int[] {1, 2, 5}));
+	public static final ChordComponent POWER = new ChordComponent( "5", stepsToOffsets(Scale.MAJOR, new int[] {1, 5}));
+	public static final ChordComponent AUGMENTED = new ChordComponent("aug", new int[] {1, 4, 8});
+	public static final ChordComponent DIMINISHED = new ChordComponent("dim", new int[] {1, 3, 6});
+	public static final ChordComponent DOM_SEVENTH = new ChordComponent("7", stepsToOffsets(Scale.MINOR, new int[] {7}));
+	public static final ChordComponent SIXTH = new ChordComponent("6", stepsToOffsets(Scale.MAJOR, new int[] {6}));
 	
 	private static final HashMap<String, ChordComponent> comps = new HashMap<String, ChordComponent>();
-	static {
-		comps.put("", MAJOR);
-		comps.put("m", MINOR);
-		comps.put("sus4", SUS4);
-		comps.put("sus2", SUS2);
-		comps.put("5", POWER);
-		comps.put("aug", AUGMENTED);
-		comps.put("dim", DIMINISHED);
-		comps.put("7", DOM_SEVENTH);
-		comps.put("6", SIXTH);
-	}
 
 	private static int[] stepsToOffsets(Scale s, int...steps) {
 		int[] offsets = new int[steps.length];
@@ -43,21 +32,33 @@ public class ChordComponent {
 	}
 	
 	private int[] offsets;
+	private String label;
 	
-	private ChordComponent(Scale scale, int... steps) {
-		offsets = new int[steps.length];
+	private ChordComponent(String label, Scale scale, int... steps) {
+		this(label, scaleStepsToHalfSteps(scale, steps));
+	}
+	
+	private static int[] scaleStepsToHalfSteps(Scale scale, int...steps) {
+		int[] offsets = new int[steps.length];
 		for (int i=0; i<steps.length; i++) {
 			offsets[i] = scale.getHalfSteps(steps[i]);
 		}
+		return offsets;
 	}
 	
-	private ChordComponent(int... offsets) {
+	private ChordComponent(String label, int... offsets) {
+		this.label = label;
 		this.offsets = offsets;
+		
+		// put new component into lookup table
+		comps.put(label, this);
 	}
 	
 	public int[] getOffsets() {
 		return offsets;
 	}
-
-
+	
+	public String getLabel() {
+		return label;
+	}	
 }
