c615b0b51f37c6a22038facd8d65aec8ff5fa23d
Stefan Tomanek <stefan@pico.ruhr.de>
2010-06-24 15:54:45 +0200

    override behaviour of back key

--- src/de/wertarbyte/chorddroid/ChordDroid.java
+++ src/de/wertarbyte/chorddroid/ChordDroid.java
@@ -8,6 +8,7 @@ import java.util.regex.Pattern;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.ContextMenu;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -229,6 +230,29 @@ public class ChordDroid extends Activity implements OnItemSelectedListener, OnCl
 			break;
 		}
 		return true;
+	}
+	
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		if (android.os.Build.VERSION.SDK_INT < android.os.Build.VERSION_CODES.ECLAIR && keyCode == KeyEvent.KEYCODE_BACK && event.getRepeatCount() == 0) {
+			// Take care of calling this method on earlier versions of
+			// the platform where it doesn't exist.
+			onBackPressed();
+		}
+		
+		return super.onKeyDown(keyCode, event);
+	}
+	
+	@Override
+	public void onBackPressed() {
+		// This will be called either automatically for you on 2.0
+		// or later, or by the code above on earlier versions of the
+		// platform.
+		if (flipper.getDisplayedChild() == 0) {
+			moveTaskToBack(false);
+		} else {
+			flipper.setDisplayedChild(0);
+		}
+		return;
 	}
 	
 }
